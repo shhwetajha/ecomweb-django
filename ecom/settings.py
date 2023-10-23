98142e659d38625596a2ecb4d02bbb176142ee8c
shhwetajha <shhwetajha9354@gmail.com>
2023-10-24 02:34:16 +0530

    updated host

--- ecom/settings.py
+++ ecom/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False,cast=bool)
 
-ALLOWED_HOSTS = ['django-env.eba-dykubvms.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-env.eba-dykubvms.us-west-2.elasticbeanstalk.com',' greatkart-web-env.eba-dykubvms.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
